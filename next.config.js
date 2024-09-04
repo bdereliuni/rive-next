2b8479295ca410d772c1387c4b9860bc23251ec3
BurakUni <21010310070@ogrenci.bartin.edu.tr>
2024-09-04 19:16:37 +0300

    build

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: "build",
+};
+
+module.exports = withPWA(nextConfig);
